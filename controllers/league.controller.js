57bbc6b1568abe5401c2f26555b06dabae778c01
Ismael Cordón <icordondominguez@gmail.com>
2024-09-11 17:28:51 +0200

    get league by Id and delete league endpoints

--- controllers/league.controller.js
+++ controllers/league.controller.js
@@ -2,13 +2,36 @@ const { response } = require('express')
 const { uploadFileCloudinary } = require('../helpers/cloudinary-actions')
 const League = require('../models/league')
 
+const getLeagueById = async(req, res = response) => {
+    const { leagueId } = req.params
+
+    try {
+        const league = await League.findById(leagueId)
+        res.status(200).json({
+            league: league
+        })
+
+    } catch(error) {
+        res.status(500).json({
+            msg: `Couldn't connect to the database. Contact the administrator`
+        })
+    }
+}
 
 const getAllLeagues = async(req, res = response) => {
-    const [leagues] = await Promise.all([League.find()])
-
-    return res.status(200).json({
-        leagues: [leagues]
-    })
+    console.log('getAllLeagues')
+    try {
+        const [leagues] = await Promise.all([League.find()])
+    
+        return res.status(200).json({
+            leagues: [leagues]
+        })
+    } catch(error) {
+        console.log(`There was an error getting all the leagues available. Error: ${error}`)
+        return res.status(500).json({
+            msg: `Couldn't connect to the database. Contact the administrator`
+        })
+    }
 }
 
 const addLeague = async (req, res = response) => {
@@ -25,7 +48,28 @@ const addLeague = async (req, res = response) => {
     })
 }
 
+const deleteLeague = async(req, res = response) => {
+    const { leagueId } = req.params
+
+    try {
+        const league = await League.findById(leagueId)
+
+        await League.findByIdAndDelete(leagueId)
+
+        return res.status(200).json({
+            msg: `League ${league.name} deleted succesfully`
+        })
+    } catch(error) {
+        console.log(`There was an error deleting leegue with id: ${leagueId} \n${error}`)
+        return res.status(500).json({
+            msg: `Couldn't connect to the database. Contact the administrator`
+        })
+    }
+}
+
 module.exports = {
-    addLeague,
-    getAllLeagues
+    getLeagueById,
+    getAllLeagues,
+    deleteLeague,
+    addLeague
 }
--- routes/league.routes.js
+++ routes/league.routes.js
@@ -1,13 +1,20 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { checkFields } = require('../middlewares/check-fields')
-const { leagueExists } = require('../helpers/database-validators')
-const { addLeague, getAllLeagues } = require('../controllers/league.controller')
+const { leagueExists, findLeagueById } = require('../helpers/database-validators')
+const { addLeague, getAllLeagues, getLeagueById, deleteLeague } = require('../controllers/league.controller')
 const { checkImageFile } = require('../middlewares/check-image-file')
 
 const router = Router()
 
 router.get('/', getAllLeagues)
+
+router.get('/:leagueId', [
+    check('leagueId', 'League id not found in request').not().isEmpty(),
+    check('leagueId', 'Provided league id is not a valid mongo id').isMongoId(),
+    check('leagueId', ).custom(findLeagueById),
+    checkFields
+], getLeagueById)
 
 router.post('/add', [
     checkImageFile,
@@ -15,5 +22,12 @@ router.post('/add', [
     check('name').custom(leagueExists),
     checkFields,
 ], addLeague)
+
+router.delete('/:leagueId', [
+    check('leagueId', 'League id not found in request').not().isEmpty(),
+    check('leagueId', 'Provided league id is not a valid mongo id').isMongoId(),
+    check('leagueId', ).custom(findLeagueById),
+    checkFields
+], deleteLeague)
 
 module.exports = router
